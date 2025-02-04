486b7df9f33246b8bd0dbe91a14146a83ba51b0b
vlaad53 <163895253+vlaad53@users.noreply.github.com>
2025-02-04 08:47:49 +0200

    js

--- js/scritp.js
+++ js/scritp.js
@@ -1,12 +1,20 @@
 const age = 16;
-console.log(typeof(age));
+console.log(age);
 const name = "Vlad";
-console.log(typeof(name));
+console.log(name);
 const isStudent = true;
-console.log(typeof(isStudent));
+console.log(isStudent);
 const  myString = "The purpose of our life is to be happy";
-console.log(typeof(myString));
+console.log(myString);
 const myNumber = 8;
-console.log(typeof(myNumber));
+console.log(myNumber);
 const myNul = null;
-console.log(typeof(myNul));
+console.log(myNul);
+const userName = prompt("Будь ласка, введіть ваше ім'я:");
+console.log(`Привіт, ${userName}!`);
+const userConfirm = confirm("Ви підтвержуєте дію?");
+if (userConfirm) {
+    console.log("Дякую, за підтверження");
+    } else {
+    console.log("Дію відінено");
+    }
